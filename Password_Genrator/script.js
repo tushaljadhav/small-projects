726d624a862142d7b0f31a6bc50bf095a6ab1447
tushaljadhav <167062306+tushaljadhav@users.noreply.github.com>
2025-12-10 13:11:36 +0000

    change the folder name and correct

---
+++ Password_Genrator/script.js
@@ -0,0 +1,29 @@
+const passwordBox = document.getElementById("password")
+const lenght = 12
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const numbers = "0123456789"
+const symbols = "!@#$%^&*()_+~`|}{[]:;?><,./-="
+const allChars = upperCase + lowerCase + numbers + symbols
+
+function createPassword(){
+    let password = ""
+    password += upperCase[Math.floor(Math.random() * upperCase.length)]
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)]
+    password += numbers[Math.floor(Math.random() * numbers.length)]
+    password += symbols[Math.floor(Math.random() * symbols.length)]
+
+    while(lenght > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)]
+
+    }
+    passwordBox.value = password
+}
+
+function copyPassword(){
+    passwordBox.select()
+    document.execCommand("copy")
+}
+
+
